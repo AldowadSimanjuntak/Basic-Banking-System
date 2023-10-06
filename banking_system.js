0d406e66c07ffa86ec0f460126b787b2a6528a7d
AldowadSimanjuntak <102914659+AldowadSimanjuntak@users.noreply.github.com>
2023-10-06 19:30:41 +0700

    Update banking_system.js
    Implementasikan kelas BankAccount dengan metode deposit() dan withdraw()
    
    Gunakan setTimeout() untuk mensimulasikan operasi transaksi yang asynchronous

--- banking_system.js
+++ banking_system.js
@@ -1,28 +1,43 @@
-// Membuat Class Bank Account
 class BankAccount {
-  constructor() {
-    this.balance = 0;
-  }
-
-  deposit(amount) {
-    setTimeout(() => {
-      if (amount > 0) {
-        this.balance += amount;
-        console.log(`Deposited: Rp.${amount}`);
+    constructor() {
+      this.balance = parseFloat(localStorage.getItem("saldo")) || 0;
+    }
+  
+    deposit(amount) {
+      const tambahan = parseFloat(amount);
+      if (!isNaN(tambahan) && tambahan > 0) {
+        alert("Tunggu sebentar, sedang update saldo..."); // Alert untuk informasi ke user sedang update saldo
+        setTimeout(() => {
+          this.balance += tambahan;
+          this.updateSaldoDisplay();
+          alert("Saldo baru Anda adalah : Rp. " + this.balance);
+          localStorage.setItem("saldo", this.balance);
+        }, 3000); // Menggunakan setTimeout untuk mensimulasikan operasi transaksi yang asynchronous (3 detik delay).
       } else {
-        console.log("Invalid deposit amount.");
+        alert("Input tidak valid. Saldo tidak berubah. Silakan masukkan angka.");
       }
-    }, 3000); // Menggunakan setTimeout untuk transaksi asynchronous (3 detik delay).
-  }
-
-  withdraw(amount) {
-    setTimeout(() => {
-      if (amount > 0 && amount <= this.balance) {
-        this.balance -= amount;
-        console.log(`Withdrawn: Rp.${amount}`);
+    }
+  
+    withdraw(amount) {
+      const pengurangan = parseFloat(amount);
+      if (!isNaN(pengurangan) && pengurangan > 0 && pengurangan <= this.balance) {
+        alert("Tunggu sebentar, sedang update saldo..."); // Alert untuk informasi ke user sedang update saldo
+        setTimeout(() => {
+          this.balance -= pengurangan;
+          this.updateSaldoDisplay();
+          alert("Saldo baru Anda adalah : Rp. " + this.balance);
+          localStorage.setItem("saldo", this.balance);
+        }, 3000); // Menggunakan setTimeout untuk mensimulasikan operasi transaksi yang asynchronous (3 detik delay).
       } else {
-        console.log("Invalid withdrawal amount or insufficient balance.");
+        alert("Input tidak valid atau saldo tidak mencukupi untuk pengurangan.");
       }
-    }, 3000); // Menggunakan setTimeout untuk transaksi asynchronous (3 detik delay).
+    }
+  
+    updateSaldoDisplay() {
+      const saldoDisplayElement = document.getElementById("saldoDisplay");
+      saldoDisplayElement.textContent = this.balance;
+    }
   }
-}
+  
+  // Membuat instance BankAccount
+  const bankAccount = new BankAccount();
